2fb37395d8ae85900f0329c67968af8346a9b0d7
vujicP <predrag.vujic@posteo.de>
2024-11-03 16:02:22 +0100

    rearrange umbrella app folder structure
    focus on single umbrella app, make it configurable if needed

--- app/forge.config.ts
+++ app/forge.config.ts
@@ -4,7 +4,7 @@ import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
 import { VitePlugin } from "@electron-forge/plugin-vite";
-import * as path from "path";
+import path from "path";
 
 const config: ForgeConfig = {
     packagerConfig: {
